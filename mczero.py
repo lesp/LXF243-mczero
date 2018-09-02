c080b10b42b4957dc17e421e983cfd56472cc34c
Les Pounder <lespounder@gmail.com>
2018-09-02 15:08:18 +0000

    Updated and testing

--- mczero.py
+++ mczero.py
@@ -2,42 +2,50 @@
 Minecraft for kids aka Minecraft Zero
 Les Pounder
 """
-
-def connect():
+try:
     from mcpi import minecraft
+    from mcpi import block
+    from time import sleep
     mc = minecraft.Minecraft.create()
-    chat("Connected")
+    print("Connected to world")
+except:
+    print("Connection Error, is Minecraft running?")
 
 def chat(msg):
     mc.postToChat(str(msg))
-
-def get_position():
-    x, y, z = mc.player.getPos()
-    print(x,y,z)
 
 def teleport(x,y,z):
     mc.player.setPos(x, y, z)
 
 def drop(height):
-    get_position()
-    mc.player.setPos(x, height, z)
+    x, y, z = mc.player.getPos()
+    mc.player.setPos(x, y+height, z)
     
 def setblock(blocktype):
-    from mcpi import block
-    get_position()
+    x, y, z = mc.player.getPos()
+    print(x,y,z)
     mc.setBlock(x, y, z, blocktype)
 
-def cube(size,blocktype)
-    from mcpi import block
-    get_position()
-    mc.setBlock(x+1, y+1, z+1, x+size, y+size, z+size, blocktype)
+def cube(size,blocktype):
+    x, y, z = mc.player.getPos()
+    mc.setBlocks(x+1, y+1, z+1, x+size, y+size, z+size, blocktype)
     
 def tnt_trail():
     x, y, z = mc.player.getPos()
     mc.setBlock(x, y, z, 46,1)
     sleep(0.1)
 
-def tnt_cube(size)
-    from mcpi import block
-    get_position()
-    mc.setBlock(x+1, y+1, z+1, x+size, y+size, z+size, 46,1)
+def tnt_cube(size):
+    x, y, z = mc.player.getPos()
+    tnt = 46
+    mc.setBlocks(x+1, y+1, z+1, x+size, y+size, z+size, tnt ,1)
+
+def pyramid(size):
+    x, y, z = mc.player.getPos()
+    for i in range(size):
+        mc.setBlocks(x+1, y+1, z+1, x+i, y+size, z+i, 41, 1)
+
+def slab(size):
+    x, y, z = mc.player.getPos()
+    for i in range(size,0,-1):
+        mc.setBlocks(x+1, y+1, z+1, x+i, y+1, z+i, 41, 1)   
